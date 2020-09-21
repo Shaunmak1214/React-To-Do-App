f569ab138e67b9a7fc11a14f60c6f4cfcf796684
Shaunmak1214 <shaonmak@gmail.com>
2020-09-21 22:44:20 +0800

    to do lists doneeee!!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Todos from './Todos'
+import AddTodo from './AddForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  
+  state = {
+
+    todos: [
+      {id: 1, content: 'wash my car'},
+      {id: 2, content: 'call my girl'}
+    ]
+
+  }
+
+  deleteTodo = (id) => {
+
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id 
+    });
+
+    this.setState({
+      todos: todos
+    })
+
+  }
+
+  addTodo = (todo) => {
+
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos: todos
+    })
+
+  }
+
+  render() {
+    return(
+      <div className="todo-app container">
+
+        <h1 className="center blue-text">Todo's</h1>
+
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+
+        <AddTodo addTodo={this.addTodo}/>
+
+      </div>
+    );
+  }
 }
 
 export default App;
