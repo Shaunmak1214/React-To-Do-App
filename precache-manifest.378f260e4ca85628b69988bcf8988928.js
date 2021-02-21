dc9a4ef09ecab35f97a43de4dded9ded82c96782
Shaunmak1214 <shaonmak@gmail.com>
2021-02-21 20:17:06 +0800

    Updates

---
+++ precache-manifest.378f260e4ca85628b69988bcf8988928.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f829a1d965d2ce1836268e67f3cdab",
+    "url": "/React-To-Do-App/index.html"
+  },
+  {
+    "revision": "53dfd37e7ec1fde6e01d",
+    "url": "/React-To-Do-App/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "a74f8381751d00b6a90f",
+    "url": "/React-To-Do-App/static/css/main.d2251587.chunk.css"
+  },
+  {
+    "revision": "53dfd37e7ec1fde6e01d",
+    "url": "/React-To-Do-App/static/js/2.428153bb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-To-Do-App/static/js/2.428153bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a74f8381751d00b6a90f",
+    "url": "/React-To-Do-App/static/js/main.cd05d562.chunk.js"
+  },
+  {
+    "revision": "4599f2a648995bd02e51",
+    "url": "/React-To-Do-App/static/js/runtime-main.fdb787c4.js"
+  },
+  {
+    "revision": "7c75dc2adaa0e29e1d8f64394cc035cd",
+    "url": "/React-To-Do-App/static/media/close.7c75dc2a.svg"
+  }
+]);
